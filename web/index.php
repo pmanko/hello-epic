f8b1a0c9b58348b9b13a9a0ce066bd6c3b8cbdd1
Piotr Mankowski <piotr.mankowski@gmail.com>
2017-01-30 22:00:29 -0800

    Hello world

--- web/index.php
+++ web/index.php
@@ -27,4 +27,10 @@ $app->get('/cowsay', function() use($app) {
   return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
 });
 
+$app->get('/hello', function() use($app) {
+  $app['monolog']->addDebug('logging output.');
+  return str_repeat('Hello', getenv('TIMES'));
+});
+
+
 $app->run();
