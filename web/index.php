7dd471c0766fcb5702b8eccc8f1e47a4805bca11
Piotr Mankowski <piotr.mankowski@gmail.com>
2017-01-31 21:10:39 -0800

    OAuth Login work

--- web/index.php
+++ web/index.php
@@ -1,8 +1,82 @@
 <?php
 require('../vendor/autoload.php');
 
+define('EPIC_API_KEY', getenv('DATABASE_URL'));
+define('EPIC_API_SECRET', getenv('DATABASE_URL'));
+// define('HEROKU_API_KEY',     '');
+// define('HEROKU_API_SECRET',  '');
+
 $app = new Silex\Application();
 $app['debug'] = true;
+
+$app->register(new Gigablah\Silex\OAuth\OAuthServiceProvider(), array(
+    'oauth.services' => array(
+        // 'EPIC' => array(
+        //     'key' => EPIC_API_KEY,
+        //     'secret' => EPIC_API_SECRET,
+        //     // 'scope' => array('email'),
+        //     // 'user_endpoint' => 'https://graph.facebook.com/me'
+        // ),
+        'Heroku' => array(
+            'key' => HEROKU_API_KEY,
+            'secret' => HEROKU_API_SECRET,
+            'scope' => array(),
+            // Note: permission needs to be obtained from Twitter to use the include_email parameter
+            'user_endpoint' => '',
+            'user_callback' => function ($token, $userInfo, $service) {
+                $token->setUser($userInfo['name']);
+                $token->setEmail($userInfo['email']);
+                $token->setUid($userInfo['id']);
+            }
+        ),
+        'Google' => array(
+            'key' => GOOGLE_API_KEY,
+            'secret' => GOOGLE_API_SECRET,
+            'scope' => array(
+                'https://www.googleapis.com/auth/userinfo.email',
+                'https://www.googleapis.com/auth/userinfo.profile'
+            ),
+            'user_endpoint' => 'https://www.googleapis.com/oauth2/v1/userinfo'
+        )        
+    )
+));
+
+
+// Provides CSRF token generation
+// You will have to include symfony/form in your composer.json
+$app->register(new Silex\Provider\FormServiceProvider());
+
+// Provides session storage
+$app->register(new Silex\Provider\SessionServiceProvider(), array(
+    'session.storage.save_path' => '/tmp'
+));
+
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+    'security.firewalls' => array(
+        'default' => array(
+            'pattern' => '^/',
+            'anonymous' => true,
+            'oauth' => array(
+                //'login_path' => '/auth/{service}',
+                //'callback_path' => '/auth/{service}/callback',
+                //'check_path' => '/auth/{service}/check',
+                'failure_path' => '/login',
+                'with_csrf' => true
+            ),
+            'logout' => array(
+                'logout_path' => '/logout',
+                'with_csrf' => true
+            ),
+            // OAuthInMemoryUserProvider returns a StubUser and is intended only for testing.
+            // Replace this with your own UserProvider and User class.
+            'users' => new Gigablah\Silex\OAuth\Security\User\Provider\OAuthInMemoryUserProvider()
+        )
+    ),
+    'security.access_rules' => array(
+        array('^/auth', 'ROLE_USER')
+    )
+));
+
 
 $dbopts = parse_url(getenv('DATABASE_URL'));
 $app->register(new Herrera\Pdo\PdoServiceProvider(),
@@ -24,6 +98,33 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 ));
 
 // Our web handlers
+$app->before(function (Symfony\Component\HttpFoundation\Request $request) use ($app) {
+    if (isset($app['security.token_storage'])) {
+        $token = $app['security.token_storage']->getToken();
+    } else {
+        $token = $app['security']->getToken();
+    }
+
+    $app['user'] = null;
+
+    if ($token && !$app['security.trust_resolver']->isAnonymous($token)) {
+        $app['user'] = $token->getUser();
+    }
+});
+
+$app->get('/login', function (Symfony\Component\HttpFoundation\Request $request) use ($app) {
+    $services = array_keys($app['oauth.services']);
+
+    return $app['twig']->render('index.twig', array(
+        'login_paths' => $app['oauth.login_paths'],
+        'logout_path' => $app['url_generator']->generate('logout', array(
+            '_csrf_token' => $app['oauth.csrf_token']('logout')
+        )),
+        'error' => $app['security.last_error']($request)
+    ));
+});
+
+$app->match('/logout', function () {})->bind('logout');
 
 $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
